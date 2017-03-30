10c8c61bbd86114882a5ea0d7cf315f1b7a48da6
Thomas Dustin Harrell <doostinharrell@gmail.com>
2017-03-29 22:58:26 -0400

    finished nav, base styles, and page config

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,20 @@
-$(document).foundation();
+(function ($) {
+
+  // Initialize foundation.
+  $(document).foundation();
+
+  // Set active link status on page load.
+  var path = location.pathname.split("/")[1];
+  if (path.length > 0) {
+    $('nav a[href^="/' + path + '"]').addClass('active');
+  }
+  else {
+    $('nav a.home').addClass('active');
+  }
+
+  // Add active class to menu-icon when the menu is open.
+  $('.menu-icon').click(function(){
+    $(this).toggleClass('active');
+  });
+
+})(jQuery);
